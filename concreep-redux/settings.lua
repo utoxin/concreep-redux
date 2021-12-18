08d54b198a8dd10c81bafaf99eda1a44b03398c0
Matthew Walker <utoxin@gmail.com>
2021-12-18 12:21:51 -0700

    Initial Import

---
+++ concreep-redux/settings.lua
@@ -0,0 +1,32 @@
+data:extend({
+    {
+        type = "int-setting",
+        name = "concreep range",
+        setting_type = "runtime-global",
+        default_value = 100,
+        minimum_value = 0,
+        maximum_value = 100,
+        order = "01"
+	},
+	{
+		type = "bool-setting",
+		name = "creep brick",
+		setting_type = "runtime-global",
+		default_value = true,
+		order = "011"
+    },
+	{
+		type = "bool-setting",
+		name = "upgrade brick",
+		setting_type = "runtime-global",
+		default_value = true,
+		order = "02"
+    },
+    {
+		type = "bool-setting",
+		name = "upgrade concrete",
+		setting_type = "runtime-global",
+		default_value = true,
+		order = "03"
+	}
+})
